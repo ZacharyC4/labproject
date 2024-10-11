12b98519f5c62e8ac85bb1cb5fe3ddcf166ae4e9
ZacharyC4 <z.louis.carter@outlook.com>
2024-10-10 20:01:58 -0600

    Lab 2
    Completed Lab 2

---
+++ app/App.jsx
@@ -0,0 +1,37 @@
+import ToDoForm from "./components/ToDoForm";
+import ToDoList from "./components/ToDoList";
+import { StyleSheet, View, Text } from "react-native";
+
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.main}>
+          < ToDoList />
+          < ToDoForm />
+        </View>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      padding: 24,
+    },
+    main: {
+      flex: 1,
+      justifyContent: "center",
+      maxWidth: 960,
+      marginHorizontal: "auto",
+    },
+    title: {
+      fontSize: 64,
+      fontWeight: "bold",
+    },
+    subtitle: {
+      fontSize: 36,
+      color: "#38434D",
+    },
+  });
+  
